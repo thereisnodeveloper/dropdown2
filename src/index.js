872b56c295d839bbbbb80f6daeca3b2ae86d9f60
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-07-27 18:31:51 -0400

    chore: Update ESLint configuration and add dropdown functionality
    The ESLint configuration has been updated to extend the "airbnb-base" rules and include the "browser" and "node" environments.
    
    Additionally, a new function `arrayToDropdown` has been added to convert an array of items into a dropdown menu. The function is documented with JSDoc comments and returns a div containing all the items as a dropdown menu. The CSS styles for the dropdown have also been added in a separate file.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
-function add(a, b) {
-  return a + b;
+/*  take array of items and convert them to a dropdown menu, returns a div
+containing all the items  */
+
+const testElem = document.createElement("div");
+
+/**
+ * Converts an array of items into a dropdown menu.
+ * @param {Array} array - The array of items to be converted.
+ * @returns {HTMLDivElement} - The div containing all the items as a dropdown menu.
+ */
+function arrayToDropdown(array) {
+  array.forEach((elem) => {
+    elem.classList.add("dropdown-item");
+  });
+  return array;
 }
 
-console.log(add(2, 3));
+//  adds CSS style for 'dropdown-item' class
+function addDropdownStyle() {
+  // Your CSS as text
+  const styles = `
+  .drop-down{
+    background-color: red;}
+
+  .drop-down.hidden{
+    display: none;}
+
+  .drop-down.shown{
+    display: block;}`;
+
+  const styleSheet = document.createElement("style");
+  styleSheet.textContent = styles;
+  document.head.appendChild(styleSheet);
+} 
+
+//  take a button and adds "dropdown" event listener to the button, returns it
+
+function addDropdownClickEvent() {}
+
+function hideDropdown(array) {}
+function showDropdown(array) {}
+
+export default arrayToDropdown;
