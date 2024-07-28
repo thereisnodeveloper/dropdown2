46cd18a46bc87329ca59d9a0910f7123dbc75774
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-07-28 13:10:56 -0400

    bug: error from ESLint for class fields

--- src/index.js
+++ src/index.js
@@ -1,43 +1,52 @@
-/*  take array of items and convert them to a dropdown menu, returns a div
-containing all the items  */
+const testElem = document.createElement('div');
 
-const testElem = document.createElement("div");
+class dropDown {
+  constructor(dropDownArray, dropDownButton) {
+    this.addDropdownStyle();
+    this.dropDownArray = dropDownArray;
+  }
+  dropDownButton = 'reference to element'
 
-/**
- * Converts an array of items into a dropdown menu.
- * @param {Array} array - The array of items to be converted.
- * @returns {HTMLDivElement} - The div containing all the items as a dropdown menu.
- */
-function arrayToDropdown(array) {
-  array.forEach((elem) => {
-    elem.classList.add("dropdown-item");
-  });
-  return array;
-}
-
-//  adds CSS style for 'dropdown-item' class
-function addDropdownStyle() {
-  // Your CSS as text
-  const styles = `
+  //  adds CSS style for 'dropdown-item' class
+  addDropdownStyle() {
+    // Your CSS as text
+    const styles = `
   .drop-down{
-    background-color: red;}
-
-  .drop-down.hidden{
-    display: none;}
+    background-color: red;
+    display: none;
+    }
 
   .drop-down.shown{
     display: block;}`;
 
-  const styleSheet = document.createElement("style");
-  styleSheet.textContent = styles;
-  document.head.appendChild(styleSheet);
-} 
+    const styleSheet = document.createElement('style');
+    styleSheet.textContent = styles;
+    document.head.appendChild(styleSheet);
+  }
 
-//  take a button and adds "dropdown" event listener to the button, returns it
+  /**
+   * Converts an array of items into a dropdown menu.
+   * @param {Array} array - The array of items to be converted.
+   * @returns {HTMLDivElement} - The div containing all the items as a dropdown menu.
+   */
+  arrayToDropdown(array) {
+    array.forEach((elem) => {
+      elem.classList.add('dropdown-item');
+    });
+    return array;
+  }
 
-function addDropdownClickEvent() {}
+  toggleDropdown(array) {
+    array.forEach((elem) => {
+      elem.classList.toggle('shown');
+    });
+  }
 
-function hideDropdown(array) {}
-function showDropdown(array) {}
+  //  take a button and adds "dropdown" event listener to the button, returns it
+  addDropdownClickEvent(button) {
+    button.addEventListener('click', this.toggleDropdown);
+    return button;
+  }
+}
 
-export default arrayToDropdown;
+export default dropDown;
